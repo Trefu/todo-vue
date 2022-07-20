b1116095781833504b29d59a34755d88c2b8da2d
Trefu_ <tre.lopez@hotmail.com>
2022-07-19 22:22:49 -0300

    componetizo navbar, agregó fetch a api mock

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import { reactive, ref } from "vue";
-const showMenu = ref(true);
+import { RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 
 </script>
 
 <template>
   <header>
-    <div>
-      <div class="bg-gray-500">
-        <nav class="container px-6 py-8 mx-auto md:flex md:justify-between md:items-center">
-          <div class="flex items-center justify-between">
-            <RouterLink to="/">
-              <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="50" height="50" />
-            </RouterLink>
-
-            <div @click="showMenu = !showMenu" class="flex md:hidden">
-              <button type="button" class="text-gray-800 hover:text-gray-400 focus:outline-none focus:text-gray-400">
-                <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-                  <path fill-rule="evenodd"
-                    d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
-                  </path>
-                </svg>
-              </button>
-            </div>
-          </div>
-
-          <ul :class="showMenu ? 'flex' : 'hidden'"
-            class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
-            <RouterLink to="/" class="text-sm font-bold text-gray-800 hover:text-blue-400">
-              Inicio
-            </RouterLink>
-            <RouterLink to="/schedule" class="text-sm font-bold text-gray-800 hover:text-blue-400">
-              Mis tareas
-            </RouterLink>
-            <RouterLink to="/done-tasks" class="text-sm font-bold text-gray-800 hover:text-blue-400">
-              Tareas completadas
-            </RouterLink>
-          </ul>
-        </nav>
-      </div>
-    </div>
+    <Navbar />
   </header>
 
   <RouterView />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,37 @@
 <script setup>
+import { onMounted, reactive, ref } from 'vue';
+import {APISettings} from '../../api/config.js'
+const loading = ref(false);
+const state = reactive({
+  todos: [
+    {
+      "a": 2
+    },
+    {
+      "a": 2
+    }
+  ]
+})
+onMounted(() => {
+  fetchTodos()
+})
+const error = ref(null);
+const fetchTodos = async () => {
+  loading.value = true;
+  const data = await fetch(APISettings.baseURL + 'todos')
+  const res = await data.json()
+  console.log(res);
+}
 </script>
 
 <template>
   <main>
-  <h1>home</h1>
+    <div v-if="loading">Loading</div>
+    <div v-if="state.todos">
+      <li v-for="(todo, i) in state.todos" :key="i">
+        {{ todo.a }}
+      </li>
+    </div>
+    <div v-if="error"> {{ error }}</div>
   </main>
 </template>
